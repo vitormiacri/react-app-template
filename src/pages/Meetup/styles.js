bb13ee1c69ed60d682fc8785b79ae08ffb065525
Vitor Morelli <vitao.miacri@gmail.com>
2019-09-04 11:26:04 -0300

    BugFixes

--- src/pages/Meetup/styles.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-  width: 100%;
-  max-width: 60em;
-  margin: 0 auto;
-`;
--- src/pages/_layouts/default/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-  min-height: 100vh;
-  background: linear-gradient(to bottom, #22202c, #402845);
-`;
